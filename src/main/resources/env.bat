85c13be3ec9ba3312e5e0596a73b691dd679c77b
Marcello Luiz Gomes do Egito Pedrosa <marcello.pedrosa@fachesf.com.br>
2025-06-28 20:17:47 -0300

    inserido opentelemetry

--- src/main/resources/env.bat
+++ src/main/resources/env.bat
@@ -1,6 +1,7 @@
 @echo off
 echo Definindo variáveis de ambiente...
 
+setx ZIPKIN_URL "http://empresa.com.br:9411/api/v2/spans" /M
 setx SERVER_LOG_HOST "logs.fachesf.com.br" /M
 setx SERVER_LOG_PORT "12201" /M
 
