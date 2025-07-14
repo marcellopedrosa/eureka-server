1c7ceae26e77e8c37f77cf871da054af64b1b77f
Marcello Luiz Gomes do Egito Pedrosa <marcello.pedrosa@fachesf.com.br>
2025-07-14 08:35:03 -0300

    ajuste na variavel de ambiente

--- env.bat
+++ env.bat
@@ -3,6 +3,7 @@ echo Definindo variaveis de ambiente DEV/local............
 
 setx ZIPKIN_URL_DEV "http://lab.fachesf.com.br:9411/api/v2/spans" /M
 setx ZIPKIN_URL_HML "http://lab.fachesf.com.br:9411/api/v2/spans" /M
+setx ZIPKIN_URL_PRD "http://lab.fachesf.com.br:9411/api/v2/spans" /M
 setx SERVER_LOG_HOST "logs.fachesf.com.br" /M
 setx SERVER_LOG_PORT "12201" /M
 
